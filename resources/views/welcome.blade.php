d668830ca99099f7444b00d8ad8e30c40986096d
charan <charanprajwal@Charans-MacBook-Pro.local>
2023-11-12 18:19:56 +0530

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,12 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">   
+
+
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Laravel</title>
 
@@ -19,6 +23,11 @@
                 font-family: 'Nunito';
             }
         </style>
+    <script>
+        var csrfToken = document.head.querySelector('meta[name="csrf-token"]').content;
+        // Use the csrfToken as needed in your JavaScript code
+        console.log(csrfToken);
+    </script>
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
